47c54075d2d0ecb40be9a73ceeea18c6c90dbbb2
Shelemeteva Anastasiia <anastasiia.shelemeteva@home24.de>
2020-09-28 16:10:25 +0200

    It draws a tree, but without a star

--- loop.go
+++ loop.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"bufio"
+	"os"
+	"strconv"
 )
 
-func main() {
-	star := "*"
-	heigth := 10
-	space := "1"
-	
 
- for i := heigth; i > 0; i-- {
-	repSpace := strings.Repeat(space, i)
+
+func main() {
+
+fmt.Print("How tall would you like to have your Christmas Tree?")	
+input := bufio.NewScanner(os.Stdin)
+input.Scan()
+height, _ := strconv.Atoi(input.Text())
+
+// in the first loop we are setting the heigth of our tree
+ for i := 1; i <= height; i++ {
+		
+// in the second loop we are printing spaces for each line
+// it means, we are starting to count at 1 and print a space every iteration until space becomes height-1
+	for space := 1; space <= height - i; space++ {
+		
+		fmt.Print(" ") 
+	}  
+// in the third loop we are printing stars for each line
+// it means, we are starting count at 1 and print a star every iteration until star becomes i*2-1
+	for star := 1; star <= i*2-1; star++ {
+		fmt.Print("x")
+	}
+// here we just print a new line
+	fmt.Println()
 	
-	fmt.Println(repSpace, star)
 	}
 
-}
+// here we print the bottom part of a tree 
+for j := 0; j < height - 1; j++ {
+	fmt.Print(" ")
+} 
+
+fmt.Println("I")
+	}
+
